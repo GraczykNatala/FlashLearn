c50bb32ec8e5e33d486cd9d21f4f1fed7c57ac4d
Natalia Graczyk <natka.graczyk@gmail.com>
2022-12-21 14:18:45 +0100

    create ExampleSet class

---
+++ src/pl/graczyk/ExampleSet.java
@@ -0,0 +1,23 @@
+package pl.graczyk;
+
+import java.util.List;
+
+public class ExampleSet {
+
+    static List<Flashcard> createExampleSet() {
+
+        Flashcard t1 = new Flashcard("Take off", "Startować");
+        Flashcard t2 = new Flashcard("Aisle seat", "Miejsce od środka");
+        Flashcard t3 = new Flashcard("Hand luggage", "Bagaż podręczny");
+        Flashcard t4 = new Flashcard("Fasten the belt", "zapiąc pasy");
+        Flashcard t5 = new Flashcard("Duty-free shop", "sklep bezcłowy");
+        Flashcard t6 = new Flashcard("Board the plane", "Wejść na pokład");
+        Flashcard t7 = new Flashcard("Gate", "Bramka");
+        Flashcard t8 = new Flashcard("Departures board", "Tablica odlotów");
+        Flashcard t9 = new Flashcard("Check in", "zameldować się");
+        List<Flashcard> testFC =  List.of(t1,t2,t3,t4,t5,t6,t7,t8,t9);
+        Main.FlashcardSets.put("testOne", testFC);
+        Main.createWinningStreak(testFC);
+
+    return testFC;}
+}
--- src/pl/graczyk/Main.java
+++ src/pl/graczyk/Main.java
@@ -4,33 +4,18 @@ import java.util.*;
 
 public class Main {
     private static final Scanner scan = new Scanner(System.in);
-    private static final Map<String, List<Flashcard> > FlashcardSets = new HashMap<>();
+    static final Map<String, List<Flashcard> > FlashcardSets = new HashMap<>();
     public static void main(String[] args) {
 
-        createTest();
+        ExampleSet.createExampleSet();
         menu();
 
     }
 
-    private static void createTest() {
-
-        Flashcard t1 = new Flashcard("Take off", "Startować");
-        Flashcard t2 = new Flashcard("Aisle seat", "Miejsce od środka");
-        Flashcard t3 = new Flashcard("Hand luggage", "Bagaż podręczny");
-        Flashcard t4 = new Flashcard("Fasten the belt", "zapiąc pasy");
-        Flashcard t5 = new Flashcard("Duty-free shop", "sklep bezcłowy");
-        Flashcard t6 = new Flashcard("Board the plane", "Wejść na pokład");
-        Flashcard t7 = new Flashcard("Gate", "Bramka");
-        Flashcard t8 = new Flashcard("Departures board", "Tablica odlotów");
-        Flashcard t9 = new Flashcard("Check in", "zameldować się");
-        List<Flashcard> testFC =  List.of(t1,t2,t3,t4,t5,t6,t7,t8,t9);
-        FlashcardSets.put("testOne", testFC);
-        createWinningStreak(testFC);
-
-        }
 
 
-    private static List<WinningStreak> createWinningStreak(List<Flashcard> fcList) {
+
+    static List<WinningStreak> createWinningStreak(List<Flashcard> fcList) {
         List<WinningStreak> setWinningStreak = new ArrayList<>(fcList.size());
         for (Flashcard o : fcList) {
             String question = o.getText();
