17d8007848561302515c61c6b4d2344b8d4c4d94
Natalia Graczyk <natka.graczyk@gmail.com>
2022-12-22 14:41:08 +0100

    apply DRY rule in Games

--- src/pl/graczyk/Games.java
+++ src/pl/graczyk/Games.java
@@ -8,10 +8,9 @@ public class Games {
 public enum GamesMenu {
     TEST(1),
     ANAGRAM(2),
-    WPISYWANIE_POPRAWNEJ_ODPOWIEDZI(3),
-    LUKI(4);
+    WPISYWANIE_POPRAWNEJ_ODPOWIEDZI(3);
 
-    private int choice;
+    private final int choice;
 
 
     GamesMenu(int choice) {
@@ -32,76 +31,79 @@ public enum GamesMenu {
         System.out.println(GamesMenu.TEST + " - " + GamesMenu.TEST.getChoice());
         System.out.println(GamesMenu.ANAGRAM + " - " + GamesMenu.ANAGRAM.getChoice());
         System.out.println(GamesMenu.WPISYWANIE_POPRAWNEJ_ODPOWIEDZI + " - " + GamesMenu.WPISYWANIE_POPRAWNEJ_ODPOWIEDZI.getChoice());
-        System.out.println(GamesMenu.LUKI + " - " + GamesMenu.LUKI.getChoice());
     }
 }
 
     public static void testGame(List<Flashcard> n) {
         boolean streak = true;
         do {
-            Flashcard question = n.get(rand.nextInt(n.size()));
+            Flashcard question = currentQuestion(n);
             String definition = question.getText();
+            String answer = question.getMeaning();
             System.out.println(definition);
-            System.out.println("odpowiedzi");
             List<String> answers = new ArrayList<>();
             answers.add(question.getMeaning());
             do {
-                Flashcard answer = n.get(rand.nextInt(n.size()));
-                if (!answers.contains(answer.getMeaning()))
-                    answers.add(answer.getMeaning());
+                Flashcard fakeAnswer = n.get(rand.nextInt(n.size()));
+                if (!answers.contains(fakeAnswer.getMeaning()))
+                    answers.add(fakeAnswer.getMeaning());
             } while (answers.size() <= 3);
+
             Collections.shuffle(answers);
-            for (int j = 0; j < answers.size(); j++) {
-                System.out.print(j);
-                System.out.println(answers.get(j) + "   ");
+            int answerNum = answers.indexOf(answer);
+            for (int j = 1; j < answers.size()+1; j++) {
+                System.out.print(j + "-" + answers.get(j-1) + "   ");
             }
             int usersAnswer = scn.nextInt();
-            if (usersAnswer == answers.indexOf(question.getMeaning())) {
-                System.out.println("BRAWO!");
-            }
-            else {
-                System.out.println("przegrałeś");
-                streak = false;
-            }
+            streak = checkAnswer(String.valueOf(usersAnswer), String.valueOf(answerNum + 1), streak);
         } while (streak);
     }
+
+
+
     public static void anagramGame(List<Flashcard> n) {
         boolean streak = true;
        do {
-           Flashcard question = n.get(rand.nextInt(n.size()));
+           Flashcard question = currentQuestion(n);
            String definition = question.getText();
-           String anagramOneWord = definition.replace(" ", "").trim();
           List<String> anagram = new ArrayList<>(List.of(definition.split("")));
            Collections.shuffle(anagram);
            String questionAnagram = anagram.toString();
            System.out.println(questionAnagram);
-           String answer = scn.nextLine();
-           if (answer.equals(definition)) {
-               System.out.println("Brawo!");
-           } else {
-               System.out.println("przegrana");
-               streak = false;
-           }
+           String usersAnswer = scn.nextLine();
+           streak = checkAnswer(usersAnswer, definition, streak);
+
        }
        while (streak);
     }
     public static void typeTheWordGame(List<Flashcard> n) {
         boolean streak = true;
         do {
-            Flashcard question = n.get(rand.nextInt(n.size()));
+            Flashcard question = currentQuestion(n);
             String definition = question.getText();
             System.out.println(definition);
             String answer = question.getMeaning();
             String usersAnswer = scn.nextLine();
-            if (usersAnswer.equals(answer)) {
-                System.out.println("Brawo!");
-            } else {
-                System.out.println("przegrana");
-                streak = false;
-            }
+            streak = checkAnswer(usersAnswer, answer, streak);
+
         }
         while (streak);
         }
 
+
+    private static boolean checkAnswer(String usersAnswer, String answer, boolean streak) {
+        if (usersAnswer.equals(answer)) {
+            System.out.println("Brawo!");
+        } else {
+            System.out.println("przegrana");
+            streak = false;
+        }
+    return streak;}
+
+    private static Flashcard currentQuestion(List<Flashcard> n) {
+        Flashcard question = n.get(rand.nextInt(n.size()));
+        String definition = question.getText();
+        String answer = question.getMeaning();
+        return new Flashcard(definition, answer);}
 }
 
